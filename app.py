090c24e94d019237d509e1de9a133a5e94002950
mazzasaverio <saverio3107@gmail.com>
2024-01-04 21:47:48 +0100

    UPDATE

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def pubsub_handler():
 
     db = SessionLocal()
     try:
-        new_item = Item(name="test")
+        new_item = Item(name="nuovotest")
         db.add(new_item)
         db.commit()
         db.refresh(new_item)
@@ -69,7 +69,7 @@ def pubsub_handler():
         items = db.query(Item).all()
         logger.info(f"######## Items: {items}")
         logger.info("Items successfully read.")
-        logger.info("<<<Items successfully read 2.")
+        logger.info("<<<Items successfully read 23333.")
         return items
     except Exception as e:
         logger.error(f"Error during database read: {e}")
